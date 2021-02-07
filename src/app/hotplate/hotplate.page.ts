d3ba8b0ad4df2cdab7712402b514932ab5cc772a
sahgon <giorgos.korn@yahoo.gr>
2021-02-07 04:40:56 +0200

    Update

--- src/app/hotplate/hotplate.page.ts
+++ src/app/hotplate/hotplate.page.ts
@@ -25,6 +25,36 @@ export class HotplatePage implements OnInit {
     }
   }
 
+  mainToSelect() {
+    document.getElementById("container").classList.add("ion-hide");
+    document.getElementById("container2").classList.remove("ion-hide");
+  }
+
+  selectToMain() {
+    document.getElementById("container2").classList.add("ion-hide");
+    document.getElementById("container").classList.remove("ion-hide");
+  }
+
+  selectToSet() {
+    document.getElementById("container2").classList.add("ion-hide");
+    document.getElementById("container3").classList.remove("ion-hide");
+  }
+
+  setToSelect() {
+    document.getElementById("container3").classList.add("ion-hide");
+    document.getElementById("container2").classList.remove("ion-hide");
+  }
+
+  setToWorking() {
+    document.getElementById("container3").classList.add("ion-hide");
+    document.getElementById("container4").classList.remove("ion-hide");
+  }
+
+  workingToMain() {
+    document.getElementById("container4").classList.add("ion-hide");
+    document.getElementById("container").classList.remove("ion-hide");
+  }
+
   ngOnInit() {
   }
 
