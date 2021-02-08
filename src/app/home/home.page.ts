a2ac55581e45095176a48ebd8d328a67dfa8daf0
sahgon <giorgos.korn@yahoo.gr>
2021-02-08 17:46:42 +0200

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,5 +15,20 @@ export class HomePage {
   ionViewDidEnter() {
     this.menu.swipeGesture(true);
   }
+
+  toggleDashboard() {
+    let d = document.getElementById("dashboard");
+    let m = document.getElementById("container");
+    let c = document.getElementById("cancelBtn");
+    if(d.classList.contains("ion-hide")) {
+      d.classList.remove("ion-hide");
+      m.classList.add("ion-hide");
+      c.classList.remove("ion-hide");
+    } else {
+      m.classList.remove("ion-hide");
+      d.classList.add("ion-hide");
+      c.classList.add("ion-hide");
+    }
+  }
   
 }
--- src/app/hotplate/hotplate.page.ts
+++ src/app/hotplate/hotplate.page.ts
@@ -29,7 +29,6 @@ export class HotplatePage implements OnInit {
       console.log("mainToSelect(), container1 doesnt contain ion-hide");
       document.getElementById("container2").classList.remove("ion-hide");
       document.getElementById("container1").classList.add("ion-hide");
-      
     }
   }
 
@@ -41,6 +40,11 @@ export class HotplatePage implements OnInit {
   selectToSet() {
     document.getElementById("container2").classList.add("ion-hide");
     document.getElementById("container3").classList.remove("ion-hide");
+  }
+
+  selectToWorking() {
+    document.getElementById("container2").classList.add("ion-hide");
+    document.getElementById("container4").classList.remove("ion-hide");
   }
 
   setToSelect() {
@@ -55,7 +59,28 @@ export class HotplatePage implements OnInit {
 
   workingToMain() {
     document.getElementById("container4").classList.add("ion-hide");
-    document.getElementById("container").classList.remove("ion-hide");
+    document.getElementById("container1").classList.remove("ion-hide");
+  }
+
+  workingToSelect() {
+    document.getElementById("container4").classList.add("ion-hide");
+    document.getElementById("container2").classList.remove("ion-hide");
+  }
+
+  transit(s: number) {
+    if (s === 1) {
+      if (this.hotplateState1 === true) this.selectToWorking();
+      else this.selectToSet();
+    } else if (s === 2) {
+      if (this.hotplateState2 === true) this.selectToWorking();
+      else this.selectToSet();
+    } else if (s === 3) {
+      if (this.hotplateState3 === true) this.selectToWorking();
+      else this.selectToSet();
+    } else if (s === 4) {
+      if (this.hotplateState4 === true) this.selectToWorking();
+      else this.selectToSet();
+    }
   }
 
   temperature: string = "0 C";
@@ -69,6 +94,53 @@ export class HotplatePage implements OnInit {
 
   duration: any;
 
+  selectedHotplate: number = 0;
+
+  selectHotplate(hotplate: number) {
+    this.selectedHotplate = hotplate;
+    console.log("Selected hotplate: " + this.selectedHotplate);
+  }
+
+  hotplateState1: boolean = false;
+  hotplateState2: boolean = false;
+  hotplateState3: boolean = false;
+  hotplateState4: boolean = false;
+
+  changeState(state: boolean) {
+    if (this.selectedHotplate === 1) {
+      this.hotplateState1 = state;
+      if (state === true) {
+        document.getElementById("hotplate1").style.backgroundColor = "red";
+      } else {
+        document.getElementById("hotplate1").style.backgroundColor = "var(--ion-color-primary)";
+      }
+    }
+    if (this.selectedHotplate === 2) {
+      this.hotplateState2 = state;
+      if (state === true) {
+        document.getElementById("hotplate2").style.backgroundColor = "red";
+      } else {
+        document.getElementById("hotplate2").style.backgroundColor = "var(--ion-color-primary)";
+      }
+    }
+    if (this.selectedHotplate === 3) {
+      this.hotplateState3 = state;
+      if (state === true) {
+        document.getElementById("hotplate3").style.backgroundColor = "red";
+      } else {
+        document.getElementById("hotplate3").style.backgroundColor = "var(--ion-color-primary)";
+      }
+    }
+    if (this.selectedHotplate === 4) {
+      this.hotplateState4 = state;
+      if (state === true) {
+        document.getElementById("hotplate4").style.backgroundColor = "red";
+      } else {
+          document.getElementById("hotplate4").style.backgroundColor = "var(--ion-color-primary)";
+      }
+    }
+    console.log("State changed");
+  }
 
   ngOnInit() {}
 }
